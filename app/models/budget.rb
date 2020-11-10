f4892f1e5f1ed65b41e805c26054dceff4b8fa1a
guy natan <guynatan@gmail.com>
2020-11-10 13:55:35 +0200

    changes

--- app/models/budget.rb
+++ app/models/budget.rb
@@ -1,6 +1,7 @@
 class Budget < ApplicationRecord
 	validates :name, presence: true
 	validates :money, presence: true
+	enum kind: [ :adding, :resetting ]
   belongs_to :garin
 	has_many :transactions, dependent: :destroy
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_08_08_183618) do
+ActiveRecord::Schema.define(version: 2020_11_10_111731) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -21,6 +21,7 @@ ActiveRecord::Schema.define(version: 2020_08_08_183618) do
     t.bigint "garin_id", null: false
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.integer "kind", default: 0
     t.index ["garin_id"], name: "index_budgets_on_garin_id"
   end
 
@@ -51,6 +52,7 @@ ActiveRecord::Schema.define(version: 2020_08_08_183618) do
     t.datetime "updated_at", precision: 6, null: false
     t.string "description"
     t.bigint "garin_id"
+    t.date "date"
     t.index ["budget_id"], name: "index_transactions_on_budget_id"
     t.index ["garin_id"], name: "index_transactions_on_garin_id"
   end
